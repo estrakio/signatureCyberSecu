6ee7579fb55720d41794864916bc51a76c6c64b9
estrakio <walterkarl@hotmail.fr>
2022-11-09 22:29:25 +0100

    Création du message et des clé

--- signature.py
+++ signature.py
@@ -1,14 +1,35 @@
-import rsa
-import hashlib
-
-publicKey, privateKey = rsa.newkeys(1024)
-
-message = input("Quel message souhaitez vous crypter ? ")
-
-signature = rsa.sign(message.encode(), privateKey, "SHA-256")
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
 
 
-verif = rsa.verify(message.encode(), signature, publicKey) == "SHA-256"
+# Script python s'occupant de générer les Clée privé et public dans les fichiers.
 
-print(f"Le message : {message} équivaut sous forme de signature  à  : {signature} ")
-print(f"\n le résultat de la vérification est: {verif} ")
+
+cle = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+)
+mdpCle = b'bonjour'
+
+clePriv = cle.private_bytes(
+    encoding = serialization.Encoding.PEM,
+    format = serialization.PrivateFormat.PKCS8,
+    encryption_algorithm = serialization.BestAvailableEncryption(mdpCle)
+)
+
+print(clePriv.splitlines())
+
+privFile = open("clePrive.pem", "w")
+privFile.write(str(clePriv.decode()))
+privFile.close()
+
+clePub = cle.public_key().public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format= serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+print(clePub)
+
+pubFile = open("clePublic.pem", "w")
+pubFile.write(str(clePub.decode()))
+pubFile.close()
